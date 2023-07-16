de8b5c8e7458bf2b00ca9d1067e49cf97305a3b9
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-16 21:27:06 +0600

    books folers added

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import app from './app';
+
+const port: number = 8000;
+const uri: any = process.env.DB_URL || 'mongodb://localhost:27017/firstdb';
+
+app.listen(port, async () => {
+   try {
+      await mongoose.connect(uri);
+      console.log('Database is connected');
+      console.log(`Example app listening on port ${port}`);
+   } catch (error) {
+      console.log('Database connect error', error);
+   }
+});
