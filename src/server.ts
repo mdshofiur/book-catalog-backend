a7653e88df0aadb8619cad462e79fbe11cf278c6
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-17 23:27:05 +0600

    api added on server file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose';
 import app from './app';
-import bookRouter from './modules/books/books.route';
+import express from 'express';
+import { addReviewToBook, createBookController, deleteBookById, getBookById, getBooksController, updateBookById } from './modules/books/books.controller';
+
 
 const port: number = 2000;
 // const uri: any = process.env.DB_URL || 'mongodb://localhost:27017/firstdb';
 const uri: any = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority"
 
-app.use(bookRouter);
+const bookRouter = express.Router();
+
+bookRouter.post('/api/books', createBookController);
+
+bookRouter.get('/api/books', getBooksController);
+
+bookRouter.get('/api/books/book/:id', getBookById);
+
+bookRouter.delete('/api/books/book/:id', deleteBookById);
+
+bookRouter.put('/api/books/book/:id', updateBookById);
+
+bookRouter.put('/api/books/book/:id/reviews', addReviewToBook);
 
 app.listen(port, async () => {
    try {
