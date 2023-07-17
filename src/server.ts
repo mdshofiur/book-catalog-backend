82b3af2b17d51a360be6f360c32ffd11c47262d4
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-17 23:04:17 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import app from './app';
 
 const port: number = 2000;
-const uri: any = process.env.DB_URL || 'mongodb://localhost:27017/firstdb';
+// const uri: any = process.env.DB_URL || 'mongodb://localhost:27017/firstdb';
+const uri: any = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority"
 
 app.listen(port, async () => {
    try {
