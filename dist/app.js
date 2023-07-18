1860a198bfa79b92e92a93ab82ec4c47a8464b0f
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-18 18:50:49 +0600

    build update

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,6 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 require("dotenv/config");
-const books_controller_1 = require("./modules/books/books.controller");
 const route_1 = __importDefault(require("./route"));
 const app = (0, express_1.default)();
 require('dotenv').config();
@@ -16,7 +15,6 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use('/api', route_1.default);
-app.get('/books/new/list', books_controller_1.getBooksController);
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
 });
