1a602bee9f66a0e2cea8933baf386cd47b8ddf83
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-18 18:17:23 +0600

    server.ts update

--- dist/server.js
+++ dist/server.js
@@ -14,15 +14,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
-const port = 2000;
-const uri = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority";
-app_1.default.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield mongoose_1.default.connect(uri);
-        console.log('Database is connected');
-        console.log(`Example app listening on port ${port}`);
-    }
-    catch (error) {
-        console.log('Database connect error', error);
-    }
-}));
+let server;
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect("mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority");
+            console.log(`Database is connected successfully`);
+            server = app_1.default.listen(1000, () => {
+                console.log(`Application  listening on port 1000`);
+            });
+        }
+        catch (err) {
+            console.log('Failed to connect database', err);
+        }
+        process.on('unhandledRejection', error => {
+            if (server) {
+                server.close(() => {
+                    process.exit(1);
+                });
+            }
+            else {
+                process.exit(1);
+            }
+        });
+    });
+}
+run();
+// import mongoose from 'mongoose';
+// import app from './app';
+// const port: number = 2000;
+// const uri: string = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority"
+// app.listen(port, async () => {
+//    try {
+//       await mongoose.connect(uri);
+//       console.log('Database is connected');
+//       console.log(`Example app listening on port ${port}`);
+//    } catch (error) {
+//       console.log('Database connect error', error);
+//    }
+// });
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,74 @@
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 
-const port: number = 2000;
-const uri: string = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority"
+
+let server: Server;
+
+async function run() {
+  try {
+    await mongoose.connect("mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority" as string);
+    console.log(`Database is connected successfully`);
+
+    server = app.listen(1000, () => {
+      console.log(`Application  listening on port 1000`);
+    });
+  } catch (err) {
+    console.log('Failed to connect database', err);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+run();
 
 
-app.listen(port, async () => {
-   try {
-      await mongoose.connect(uri);
-      console.log('Database is connected');
-      console.log(`Example app listening on port ${port}`);
-   } catch (error) {
-      console.log('Database connect error', error);
-   }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from 'mongoose';
+// import app from './app';
+
+// const port: number = 2000;
+// const uri: string = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority"
+
+
+// app.listen(port, async () => {
+//    try {
+//       await mongoose.connect(uri);
+//       console.log('Database is connected');
+//       console.log(`Example app listening on port ${port}`);
+//    } catch (error) {
+//       console.log('Database connect error', error);
+//    }
+// });
