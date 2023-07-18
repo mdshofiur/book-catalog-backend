64f6c3e880577c250afda7d00d4cfcfe5d75e422
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-18 15:53:02 +0600

    delete app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
+import express, { Express} from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
 import mongoose from 'mongoose';
-import express from 'express';
-import app from './app';
-import bookRouter from './modules/books/books.route';
+import { addReviewToBook, createBookController, deleteBookById, getBookById, getBooksController, updateBookById } from './modules/books/books.controller';
+
+const app: Express = express();
+require('dotenv').config();
 
 const port: number = 2000;
 const uri: string = "mongodb+srv://testingDatabase:LmlkuPM6zWk6hdW5@cluster0.e7yhr.mongodb.net/cow-hurt?retryWrites=true&w=majority"
 
-app.use('/api/books', bookRouter); // Mount the bookRouter under '/api/books' path
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.post('/api/books', createBookController);
+app.get('/api/books', getBooksController);
+app.get('/api/books/:id', getBookById);
+app.delete('/api/books/:id', deleteBookById);
+app.put('/api/books/:id', updateBookById);
+app.put('/api/books/:id/reviews', addReviewToBook);
 
 app.listen(port, async () => {
    try {
